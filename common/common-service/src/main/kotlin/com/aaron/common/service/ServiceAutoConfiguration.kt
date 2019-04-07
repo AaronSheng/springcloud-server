fac5f92cc4d3f4a191d48e149b48c9a3a54b5e85
Aaron@ShengMBP <shengmac@126.com>
2019-04-07 20:29:39 +0800

    common-service统一security配置

---
+++ common/common-service/src/main/kotlin/com/aaron/common/service/ServiceAutoConfiguration.kt
@@ -0,0 +1,24 @@
+package com.aaron.common.service
+
+import com.aaron.common.service.util.SpringContextUtil
+import org.springframework.boot.autoconfigure.AutoConfigureBefore
+import org.springframework.boot.autoconfigure.AutoConfigureOrder
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient
+import org.springframework.cloud.consul.ConsulAutoConfiguration
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.PropertySource
+import org.springframework.core.Ordered
+
+/**
+ * Created by Aaron Sheng on 2019/4/7.
+ */
+@Configuration
+@PropertySource("classpath:/common-service.properties")
+@AutoConfigureOrder(Ordered.HIGHEST_PRECEDENCE)
+@AutoConfigureBefore(ConsulAutoConfiguration::class)
+@EnableDiscoveryClient
+class ServiceAutoConfiguration {
+    @Bean
+    fun springContextUtil() = SpringContextUtil()
+}
