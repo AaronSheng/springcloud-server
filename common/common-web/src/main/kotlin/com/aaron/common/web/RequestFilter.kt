06b5bb993a880121aa9c2911dede4daf30ca8905
Aaron@ShengMBP <shengmac@126.com>
2019-04-07 20:30:51 +0800

    common-web添加RequestFilter和ReponseFilter注解

---
+++ common/common-web/src/main/kotlin/com/aaron/common/web/RequestFilter.kt
@@ -0,0 +1,15 @@
+package com.aaron.common.web
+
+import org.springframework.beans.factory.config.ConfigurableBeanFactory
+import org.springframework.context.annotation.Scope
+import org.springframework.stereotype.Component
+
+/**
+ * Created by Aaron Sheng on 2019/4/7.
+ */
+@Target(AnnotationTarget.CLASS, AnnotationTarget.FILE)
+@Retention(AnnotationRetention.RUNTIME)
+@MustBeDocumented
+@Component
+@Scope(ConfigurableBeanFactory.SCOPE_SINGLETON)
+annotation class RequestFilter
